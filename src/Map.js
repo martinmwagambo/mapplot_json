f8971124b4d32a5ca25c2afebac961695f104814
Martin Mwagambo <87171117+martinmwagambo@users.noreply.github.com>
2024-02-15 16:24:43 +0300

    ..

--- src/Map.js
+++ src/Map.js
@@ -53,7 +53,7 @@ class Map extends Component {
     this.markers = data.map(({ latitude, longitude, location }) => {
       const icon = L.divIcon({
         className: "hero-icon", // Define a class for the icon
-        html: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" /></svg>`
+        html: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" /></svg>`,
       });
 
       return L.marker([parseFloat(latitude), parseFloat(longitude)], { icon })
